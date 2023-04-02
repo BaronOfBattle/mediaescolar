603f479dde0aa53f1842a6b8bf69abf550316a21
Harrison Gonçalves <brzeirogamer@gmail.com>
2023-04-01 21:14:37 -0300

    versao final

--- MediaEscolar/Apresentacao/Entrar.cs
+++ MediaEscolar/Apresentacao/Entrar.cs
@@ -37,15 +37,21 @@ namespace MediaEscolar.Apresentacao
             Conexao con = new Conexao();
             controle.acessar(txbMatricula.Text, txbSenha.Text);
             int tipoUsuario = controle.getTipoUsuario(txbMatricula.Text);
+
+            if (!int.TryParse(txbMatricula.Text, out int matricula))
+            {
+                MessageBox.Show("Digite uma matrícula válida", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
             SqlCommand cmd = new SqlCommand();
             string query = "SELECT nome,matricula FROM logins WHERE matricula = @matricula";
-            cmd.Parameters.AddWithValue("@matricula", txbMatricula.Text);
+            cmd.Parameters.AddWithValue("@matricula", matricula);
             cmd.CommandText = query;
             cmd.Connection = con.Conectar();
             con.Conectar();
 
             SqlDataReader reader = cmd.ExecuteReader();
-
             if (reader.Read())
             {
                 controle.nomeUsuario = reader["nome"].ToString();
